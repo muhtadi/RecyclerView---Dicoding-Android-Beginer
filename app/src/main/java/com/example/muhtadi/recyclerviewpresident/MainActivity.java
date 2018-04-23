91b69e265afa4e386e8dd87601129531b3566ac9
Muhtadi Irfan <muhtadi.irfan@gmail.com>
2018-04-23 13:58:17 +0700

    update MainActivity for use Item click handler

--- app/src/main/java/com/example/muhtadi/recyclerviewpresident/MainActivity.java
+++ app/src/main/java/com/example/muhtadi/recyclerviewpresident/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -30,5 +32,16 @@ public class MainActivity extends AppCompatActivity {
         ListPresidentAdapter listPresidentAdapter = new ListPresidentAdapter(this);
         listPresidentAdapter.setListPresident(list);
         rvCategory.setAdapter(listPresidentAdapter);
+
+        ItemClickSupport.addTo(rvCategory).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
+            @Override
+            public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                showSelectedPresident(list.get(position));
+            }
+        });
+    }
+
+    private void showSelectedPresident(President president){
+        Toast.makeText(this, "Kamu memilih "+president.getName(), Toast.LENGTH_SHORT).show();
     }
 }
