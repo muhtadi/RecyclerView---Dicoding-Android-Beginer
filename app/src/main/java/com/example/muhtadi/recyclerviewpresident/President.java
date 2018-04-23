2126f83edc4ccba6ea15f50d343d3749a151e574
Muhtadi Irfan <muhtadi.irfan@gmail.com>
2018-04-23 13:28:26 +0700

    add President getter and setter

---
+++ app/src/main/java/com/example/muhtadi/recyclerviewpresident/President.java
@@ -0,0 +1,38 @@
+package com.example.muhtadi.recyclerviewpresident;
+
+/**
+ * Created by Muhtadi on 23/04/2018.
+ */
+
+public class President {
+    private String name;
+    private String remarks;
+    private String photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+
+}
+
